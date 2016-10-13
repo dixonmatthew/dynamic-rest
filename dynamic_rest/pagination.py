f73e6caccb4b6c522675944b78a355d1b175945f
dixonmatthew <matthewjdixon@gmail.com>
2016-10-13 14:41:23 +0100

    Add link fields to default metadata.
    This was the only behaviour missing when I switched my serializers over from DRF to DREST so I am proposing it as new default. Under DRF I used a custom pagination class referenced in settings.py, but I don't think that is possible in DREST..?

--- dynamic_rest/pagination.py
+++ dynamic_rest/pagination.py
@@ -24,6 +24,8 @@ class DynamicPageNumberPagination(PageNumberPagination):
             'total_pages': self.page.paginator.num_pages,
             'page': self.page.number,
             'per_page': self.get_page_size(self.request)
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
         }
 
     def get_paginated_response(self, data):
